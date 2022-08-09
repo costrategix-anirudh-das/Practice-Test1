0bc3d29b72a623fbb97c68041d759c23ed1c6948
costrategix-anirudh-das <anirudh.das@costrategix.com>
2022-08-09 18:48:15 +0530

    add and sub

--- src/first_steps/Addition.java
+++ src/first_steps/Addition.java
@@ -1,10 +1,24 @@
 package first_steps;
 
 public class Addition {
-		public static void main(String[] args) {
-			int a,b;
-			a=1;
-			b=2;
-			System.out.println(a+b);
-		}
+		
+	public void add() {
+		int a,b;
+		a=1;
+		b=2;
+		System.out.println(a+b);
+	}
+	
+	public void sub() {
+		int a,b;
+		a=1;
+		b=2;
+		System.out.println(a-b);
+	}
+	
+	public static void main(String[] args) {
+		Addition obj=new Addition();
+		obj.add();
+		obj.sub();
+	}
 }
