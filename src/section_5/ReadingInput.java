bc267152a008a9e77cc8999e1b93512c1d2f9faf
costrategix-anirudh-das <anirudh.das@costrategix.com>
2022-08-12 03:53:39 +0530

    ReadInput_Challenge

--- src/section_5/ReadingInput.java
+++ src/section_5/ReadingInput.java
@@ -4,17 +4,28 @@ import java.util.*;
 public class ReadingInput {
 	
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    	Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter your year of birth:");
-        int yearOfBirth = scanner.nextInt();
-        scanner.nextLine(); //takes enter after we give first value 
 
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
-        int age = 2022 - yearOfBirth;
+        boolean hasNextInt = scanner.hasNextInt();
 
-        System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+        if(hasNextInt) {
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine(); // takes enter after giving first input
+
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
+            int age = 2022 - yearOfBirth;
+
+            if(age >= 0 && age <= 100) {
+                System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+            } else {
+                System.out.println("Invalid year of birth");
+            }
+        } else {
+            System.out.println("Unable to parse year of birth.");
+        }
 
         scanner.close();
     }
