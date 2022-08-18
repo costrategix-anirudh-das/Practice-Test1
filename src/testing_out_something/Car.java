eae7b3cc9b2376dafa4a352ac9688a4085087659
costrategix-anirudh-das <anirudh.das@costrategix.com>
2022-08-18 11:48:51 +0530

    Challenges

---
+++ src/testing_out_something/Car.java
@@ -0,0 +1,10 @@
+package testing_out_something;
+
+public class Car {
+	
+	public static String doors() {
+		return "Hello";
+	}
+	
+
+}
---
+++ src/testing_out_something/Player.java
@@ -0,0 +1,11 @@
+package testing_out_something;
+
+public class Player {
+	public static void main(String[] args) {
+		
+		Game op = new Game();
+		System.out.println(op.a);
+		op.a = 50;
+		System.out.print(op.a);
+	}
+}
